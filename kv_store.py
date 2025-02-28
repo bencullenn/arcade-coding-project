e035d87c34bbe03eaee149b76eebf10831f8f420
Ben Cullen <ben.cullen.n@gmail.com>
2025-02-28 13:29:55 -0700

    Created basic version of key value store

--- kv_store.py
+++ kv_store.py
@@ -1,21 +1,29 @@
 class Store:
-    def __init__(self):
-        pass
+    def __init__(self, key_type, value_type):
+        self.data = {}
+        self.key_type = key_type
+        self.value_type = value_type
+        self.pending_changes = []
 
     def set(self, key, value):
-        pass
+        self.pending_changes.append(("set", key, value))
 
     def get(self, key):
-        pass
+        return self.data.get(key)
 
     def delete(self, key):
-        pass
+        self.pending_changes.append(("delete", key))
 
     def begin(self):
-        pass
+        self.pending_changes = []
 
     def commit(self):
-        pass
+        for change in self.pending_changes:
+            if change[0] == "set":
+                self.data[change[1]] = change[2]
+            elif change[0] == "delete":
+                del self.data[change[1]]
+        self.pending_changes = []
 
     def rollback(self):
-        pass
+        self.pending_changes = []
